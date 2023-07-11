b1a70afd744075340ee3ec55a678e02d61722464
Igor Piatkowski <igor.piatkowski@gmail.com>
2023-07-11 21:48:21 +0200

    transposing the list

---
+++ Chapter 4/Character Picture Grid.py
@@ -0,0 +1,15 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+for i in range(len(grid[0])): #range is the number of columns
+    for j in range(len(grid)): #range is the number of rows
+        print(grid[j][i],end='') #transposing the grid
+    print('')
